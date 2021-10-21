fee5397c66044ccba19b582d988cffaa13620a17
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-20 21:21:45 -0400

    updated functions and added questions

--- db/connection.js
+++ db/connection.js
@@ -9,17 +9,38 @@ const db = mysql.createConnection(
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "employees_db",
-  },
-  console.log(`Connected to the employees_db database.`)
+  }
+  // console.log(`Connected to the employees_db database.`)
 );
 
 // TODO: add validation and handle duplicates better
 const addStuff = (type, obj) => {
   switch (type) {
     case "department":
-      const sql = `INSERT INTO departments (name) VALUES (?)`;
-      const params = [obj];
-      db.query(sql, params, (err, result) => {
+      let dsql = `INSERT INTO departments (name) VALUES (?)`;
+      const dparams = [obj];
+      db.query(dsql, dparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+    case "role":
+      let rsql = `INSERT INTO roles (title, salary, department_id) VALUES (?)`;
+      const rparams = [obj];
+      db.query(rsql, rparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+    case "employee":
+      let esql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?)`;
+      if (obj[3] == 0) {
+        obj[3] = null;
+      }
+      const eparams = [obj];
+      db.query(esql, eparams, (err, result) => {
         if (err) {
           console.log(err);
         }
@@ -28,51 +49,61 @@ const addStuff = (type, obj) => {
   }
 };
 
-let output = [];
-const getDepartments = () => {
-  const sql = "SELECT name FROM departments";
-  // let something = await db.promise().query(sql);
-  // let arr1 = [];
-  // something[0].forEach((element) => arr1.push(element.name));
-
-  // // console.log(arr1);
-  // return arr1;
-return db.promise().query(sql);
-
-        // db.query(sql, (err, result) => {
-        //   if (err) {
-        //     console.log(err);
-        //   }
-
-        //     let output = [];
-        //     result.forEach(element => output.push(element.name));
-        //   console.log(output);
-
-        // });
-// return db.query(sql);
-}
-// console.log(getDepartments());
-const getADepartment = () => {
-  getDepartments().then((res) => {
-    // console.log(res);
-  });
+const deleteStuff = (type, obj) => {
+  switch (type) {
+    case "department":
+      let dsql = `DELETE FROM departments WHERE id = ?`;
+      const dparams = [obj];
+      db.query(dsql, dparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+    case "role":
+      let rsql = `DELETE FROM roles WHERE id = ?`;
+      const rparams = [obj];
+      db.query(rsql, rparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+    case "employee":
+      let esql = `DELETE FROM employees WHERE id = ?`;
+      const eparams = [obj];
+      db.query(esql, eparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+  }
 };
-console.log(getADepartment());
-// console.log(getDepartments());
-// const output = getDepartments().then((res) => {
-//   // let arr1 = [];
-//   //     res[0].forEach(element => arr1.push(element.name));
 
-//   // // console.log(res[0]);
-//   // return arr1;
-// });
-  //  console.log(output);
-// const departments = db.query(sql, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   let output = [];
-//   result.forEach((element) => output.push(element.name));
-//   console.log(output);
-// });
-module.exports = { addStuff, getDepartments };
+const updateStuff = (type, obj) => {
+  switch (type) {
+    case "manager":
+      if (obj[0] == 0) {
+        obj[0] = null;
+      }
+      let msql = `UPDATE employees SET manager_id = ${obj[0]} WHERE id = ${obj[1]}`;
+      // const mparams = [obj];
+      db.query(msql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+    case "role":
+      let rsql = `UPDATE employees SET role_id = ${obj[0]} WHERE id = ${obj[1]}`;
+      const rparams = [obj];
+      db.query(rsql, rparams, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      break;
+  }
+};
+module.exports = { addStuff, deleteStuff, updateStuff };
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const inquirer = require("inquirer");
-const { addStuff } = require("./db/connection");
+const { addStuff, deleteStuff, updateStuff } = require("./db/connection");
 const { firstQuestions, addDepartmentQuestion } = require("./lib/questions");
-
+//use dotenv to hide mysql variables
+require("dotenv").config();
+const mysql = require("mysql2");
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "employees_db",
+  }
+  // console.log(`Connected to the employees_db database.`)
+);
 const firstPrompt = () => {
   inquirer.prompt(firstQuestions).then((answer) => {
     console.log(answer.choose);
@@ -45,6 +57,9 @@ const firstPrompt = () => {
       case "View Employees By Department":
         view("employees");
         break;
+      case "Exit":
+        process.exit();
+        break;
     }
   });
 };
@@ -61,12 +76,295 @@ const addA = (type) => {
       });
       break;
     case "role":
-      inquirer.prompt(addDepartmentQuestion).then((answer) => {
-        addStuff("department", answer.departmentName);
-        console.log(
-          `The ${answer.departmentName} department has been created.`
+      const rsql = "SELECT id, name FROM departments";
+      db.query(rsql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let output = [];
+        result.forEach((element) =>
+          output.push(`${element.id}: ${element.name}`)
         );
-        firstPrompt();
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Role title?",
+              name: "roleTitle",
+            },
+            {
+              type: "input",
+              message: "Role salary?",
+              name: "roleSalary",
+            },
+            {
+              type: "list",
+              message: "Choose department:",
+              name: "roleDepartment",
+              choices: output,
+            },
+          ])
+          .then((answer) => {
+            let role_dept = answer.roleDepartment.substring(
+              0,
+              answer.roleDepartment.indexOf(":")
+            );
+            addStuff("role", [answer.roleTitle, answer.roleSalary, role_dept]);
+            console.log(`The ${answer.roleTitle} role has been created.`);
+            firstPrompt();
+          });
+      });
+      break;
+    case "employee":
+      const esql =
+        "SELECT * FROM employees JOIN roles ON employees.role_id = roles.id";
+      db.query(esql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let manager = [`0 - None`];
+        result.forEach((element) =>
+          manager.push(
+            `${element.id}: ${element.first_name} ${element.last_name}`
+          )
+        );
+        let role = [];
+        result.forEach((element) =>
+          role.push(`${element.id}: ${element.title}`)
+        );
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "First name?",
+              name: "firstName",
+            },
+            {
+              type: "input",
+              message: "Last name?",
+              name: "lastName",
+            },
+            {
+              type: "list",
+              message: "Choose role:",
+              name: "employeeRole",
+              choices: role,
+            },
+            {
+              type: "list",
+              message: "Choose manager:",
+              name: "roleManager",
+              choices: manager,
+            },
+          ])
+          .then((answer) => {
+            let emp_role = answer.employeeRole.substring(
+              0,
+              answer.employeeRole.indexOf(":")
+            );
+            let role_man = answer.roleManager.substring(
+              0,
+              answer.roleManager.indexOf(":")
+            );
+            addStuff("employee", [
+              answer.firstName,
+              answer.lastName,
+              emp_role,
+              role_man,
+            ]);
+            console.log(
+              `The employee ${answer.firstName} ${answer.lastName} has been added.`
+            );
+            firstPrompt();
+          });
+      });
+      break;
+  }
+};
+const deleteA = (type) => {
+  switch (type) {
+    case "department":
+      const dsql = "SELECT id, name FROM departments";
+      db.query(dsql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let output = [];
+        result.forEach((element) =>
+          output.push(`${element.id}: ${element.name}`)
+        );
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Choose department:",
+              name: "deleteDepartment",
+              choices: output,
+            },
+          ])
+          .then((answer) => {
+            let dept_id = answer.deleteDepartment.substring(
+              0,
+              answer.deleteDepartment.indexOf(":")
+            );
+            deleteStuff("department", [dept_id]);
+            console.log(
+              `The ${answer.deleteDepartment} department has been deleted.`
+            );
+            firstPrompt();
+          });
+      });
+      break;
+    case "role":
+      const rsql = "SELECT id, title FROM roles";
+      db.query(rsql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let output = [];
+        result.forEach((element) =>
+          output.push(`${element.id}: ${element.title}`)
+        );
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Choose department:",
+              name: "deleteRole",
+              choices: output,
+            },
+          ])
+          .then((answer) => {
+            let role_id = answer.deleteRole.substring(
+              0,
+              answer.deleteRole.indexOf(":")
+            );
+            deleteStuff("role", [role_id]);
+            console.log(`The ${answer.deleteRole} role has been deleted.`);
+            firstPrompt();
+          });
+      });
+      break;
+    case "employee":
+      const esql = "SELECT * FROM employees";
+      db.query(esql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let employees = [];
+        result.forEach((element) =>
+          employees.push(
+            `${element.id}: ${element.first_name} ${element.last_name}`
+          )
+        );
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Choose employee:",
+              name: "deleteEmployee",
+              choices: employees,
+            },
+          ])
+          .then((answer) => {
+            let emp_id = answer.deleteEmployee.substring(
+              0,
+              answer.deleteEmployee.indexOf(":")
+            );
+            deleteStuff("employee", [
+              answer.deleteEmployee.substring(
+                0,
+                answer.deleteEmployee.indexOf(":")
+              ),
+            ]);
+            console.log(
+              `The employee ${answer.deleteEmployee} has been deleted.`
+            );
+            firstPrompt();
+          });
+      });
+      break;
+  }
+};
+
+const updateA = (type) => {
+  switch (type) {
+    case "manager":
+      const esql =
+        "SELECT * FROM employees JOIN roles ON employees.role_id = roles.id";
+      db.query(esql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let employee = [`0 - None`];
+        result.forEach((element) =>
+          employee.push(
+            `${element.id}: ${element.first_name} ${element.last_name}`
+          )
+        );
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Choose employee:",
+              name: "chosenEmployee",
+              choices: employee,
+            },
+            {
+              type: "list",
+              message: "Choose manager:",
+              name: "chosenManager",
+              choices: employee,
+            },
+          ])
+          .then((answer) => {
+            let chosen_employee = answer.chosenEmployee.substring(
+              0,
+              answer.chosenEmployee.indexOf(":")
+            );
+            let chosen_manager = answer.chosenManager.substring(
+              0,
+              answer.chosenManager.indexOf(":")
+            );
+            updateStuff("manager", [
+              chosen_manager,
+              chosen_employee,
+            ]);
+            console.log(
+              `${answer.chosenEmployee}'s new manager is ${answer.chosenManager}.`
+            );
+            firstPrompt();
+          });
+      });
+      break;
+    case "role":
+      const rsql = "SELECT id, title FROM roles";
+      db.query(rsql, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        let output = [];
+        result.forEach((element) =>
+          output.push(`${element.id}: ${element.title}`)
+        );
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Choose department:",
+              name: "deleteRole",
+              choices: output,
+            },
+          ])
+          .then((answer) => {
+            let role_id = answer.deleteRole.substring(
+              0,
+              answer.deleteRole.indexOf(":")
+            );
+            deleteStuff("role", [role_id]);
+            console.log(`The ${answer.deleteRole} role has been deleted.`);
+            firstPrompt();
+          });
       });
       break;
   }
